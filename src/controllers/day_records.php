7f41a1ca714eeeb811320f644fe91ec1beca6a70
João Victor <joaovictorpassis@gmail.com>
2022-02-21 16:49:01 -0300

    show calculated hours

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -6,12 +6,4 @@ loadModel('WorkingHours');
 
 $date = (new DateTime())->getTimestamp();
 $today = strftime('%b %dth, %Y', $date);
-
-$user = $_SESSION['user'];
-$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
-
-
-loadTemplateView('day_records',[
-    'today' => $today,
-    'records' => $records
-]);
+loadTemplateView('day_records', ['today' => $today]);
--- src/views/day_records.php
+++ src/views/day_records.php
@@ -14,12 +14,12 @@
             </div>
             <div class="card-body">
                 <div class="d-flex m-5 justify-content-around">
-                    <span class="record">Entry 1: <?= $records->time1 ?? '---' ?></span>
-                    <span class="record">Exit 1: <?= $records->time2 ?? '---' ?></span>
+                    <span class="record">Entry 1: <?= $workingHours->time1 ?? '---' ?></span>
+                    <span class="record">Exit 1: <?= $workingHours->time2 ?? '---' ?></span>
                 </div>
                 <div class="d-flex m-5 justify-content-around">
-                    <span class="record">Entry 2: <?= $records->time3 ?? '---' ?></span>
-                    <span class="record">Exit 2: <?= $records->time4 ?? '---' ?></span>
+                    <span class="record">Entry 2: <?= $workingHours->time3 ?? '---' ?></span>
+                    <span class="record">Exit 2: <?= $workingHours->time4 ?? '---' ?></span>
                 </div>
             </div>
             <div class="card-footer d-flex justify-content-center">
